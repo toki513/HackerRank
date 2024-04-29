aceb8a4ca805a083d04d13f1b3f9f37558bbdd74
Toki Tahmid Khan <122171015+toki513@users.noreply.github.com>
2024-04-29 18:46:16 +0600

    Update Compare the Triplets.cpp

--- Compare the Triplets.cpp
+++ Compare the Triplets.cpp
@@ -35,14 +35,6 @@ string ltrim(const string &);
 string rtrim(const string &);
 vector<string> split(const string &);
 
-/*
- * Complete the 'compareTriplets' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER_ARRAY b
- */
 
 vector<int> compareTriplets(vector<int> a, vector<int> b) {
     int aSum = 0, bSum = 0;
