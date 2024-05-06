b24000384248df93ef4ab7fda05e4747c096be7f
toki513 <tokitahmidkhan@gmail.com>
2024-05-06 23:00:58 +0600

    Solved

--- Pangrams.cpp
+++ Pangrams.cpp
@@ -1,24 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void removeDup(char str[], int n){
+    int i, j , index = 0;
+    for(i=0;i<n;i++){
+        for(j=0;j<i;j++){
+            if(str[i] == str[j])
+                break;
+        }
+        if(j==i){
+            index++;
+    }
+    }   
+    cout<<index<<endl;
+    if(index == 26){
+        cout<<"Pnagrams";
+    }
+        else{
+            cout<<"Not Pangrams";
+        }
+    }
 
 int main()
 {
 
-    string s;
-    getline(cin, s);
-    cout<<s<<endl;
-    int i,count = 0;
-    cout<<s.length()<<endl;
-    for(i=0;i<s.length();i++){
-        if(s[i] == 'a' || 'b'||'c'||'d'|| 'e'||'f' || 'g'||'h'||'i'|| 'j'||'k' || 'l'||'m'||'n'|| 'o'||'p' || 'q'||'r'||'s'|| 't'||'u' || 'v'||'w'||'x'|| 'y'||'z'||'A' || 'B'||'C'||'D'|| 'E'||'F' || 'G'||'H'||'I'|| 'J'||'K' || 'L'||'N'||'M'|| 'O'||'P' || 'Q'||'R'||'S'|| 'T'||'U' || 'V'||'W'||'X'|| 'Y'||'Z'){
-                count++;
-        } else{count = 0;}
-    } cout<<count<<endl;
-    if(count== 26) 
-        {cout<< "pangram";}
-    else{
-         cout<<"not pangram";
-    return 0;
-}
+    char str[] = "a quick brown fox jumps over the lazy dog";
+    int n = sizeof(str)/sizeof(str[0]);
+
+    removeDup (str, n);
 }
